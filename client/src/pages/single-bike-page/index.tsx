914162b1eeb0391fe8405f83c4bec6cbb4414116
rokas063 <rokasvertelis@gmail.com>
2023-02-24 22:32:11 +0200

    init: added height,weight into slider

--- client/src/pages/single-bike-page/index.tsx
+++ client/src/pages/single-bike-page/index.tsx
@@ -5,6 +5,7 @@ import { useParams, Navigate } from 'react-router-dom';
 import ApiService from 'services/api-service';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import Img from 'components/ui/img';
 
 const SingleBikePage = () => {
   const { id } = useParams();
@@ -20,11 +21,27 @@ const SingleBikePage = () => {
   }, []);
 
   if (id === undefined) return <Navigate to={routes.HomePage} />;
+  if (bike === undefined) return null;
 
   return (
     <Box component="pre">
       {JSON.stringify(bike, null, 4)}
+      <Box sx={{ width: 700, height: 400, pl: 10 }}>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {bike.images.map((img) => (
+          <SwiperSlide>
+            <Img src=(img) sx={{ width: 1, height: 1}} />
+          </SwiperSlide>
+        ))}
+        ...
+      </Swiper>
     </Box>
+    </box>
   );
 };
 
