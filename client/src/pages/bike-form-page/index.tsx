3a34ac01d3e84d58233f246729a6ae7cf739c6de
rokas063 <rokasvertelis@gmail.com>
2023-02-25 19:23:40 +0200

    init: form start

--- client/src/pages/bike-form-page/index.tsx
+++ client/src/pages/bike-form-page/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
-
+import {
+    Stack,
+    Typography,
+    TextField,
+    Box,
+    Stack
+    Paper,
+    InputAdornment,
+    IconButton,
+} from '@mui/material'
 const BikesFormPage = () => (
-  <div>BikesFormPage</div>
-);
+    <Stack>
+        <Stack component={form}>
+            <Typography variant='h5'>Pridėkite naują prekę</Typography>
+            <TextField
+        </Stack>
+
+
+
+    </Stack>
 
 export default BikesFormPage;
