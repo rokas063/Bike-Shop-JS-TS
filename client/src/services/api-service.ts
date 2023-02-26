b079c177f200ee84530be948b4ae100aadd166e8
rokas063 <rokasvertelis@gmail.com>
2023-02-26 07:20:26 +0200

    init: Added bikesmodel info in api service

--- client/src/services/api-service.ts
+++ client/src/services/api-service.ts
@@ -2,29 +2,48 @@ import axios from 'axios';
 
 const API_BASE_URL = 'http://localhost:5024';
 
+type BikesModel = {
+  id: string;
+  title: string;
+  country: string;
+  city: string;
+  images: Image[];
+  price: string;
+};
+
+type Image = {
+  id: number;
+  url: string;
+};
+
 const ApiService = {
-  async fetchBikes() {
+  async fetchBikesList() {
     const response = await axios.get<BikesModel[]>(`${API_BASE_URL}/bikes`);
     return response.data;
   },
 
-  async fetchBikes(id: string | number) {
+  async fetchBikesById(id: string | number) {
     const response = await axios.get<BikesModel>(`${API_BASE_URL}/bikes/${id}`);
     return response.data;
   },
 
-  async createBikes(bikeData: any) {
-    const response = await axios.post(`${API_BASE_URL}/bikes`, bikeData);
+  async createBikes(bikesData: any) {
+    const response = await axios.post(`${API_BASE_URL}/bikes`, bikesData);
     return response.data;
   },
 
-  async updateBikes(id: string | number, bikeData: any) {
-    const response = await axios.put(`${API_BASE_URL}/bikes/${id}`, bikeData);
+  async updateBikes(id: string | number, bikesData: any) {
+    const response = await axios.put(`${API_BASE_URL}/bikes/${id}`, bikesData);
     return response.data;
   },
 
   async deleteBikes(id: string | number) {
     const response = await axios.delete(`${API_BASE_URL}/bikes/${id}`);
+    return response.data;
+  },
+
+  async fetchBikes() {
+    const response = await axios.get<BikesModel[]>(`${API_BASE_URL}/bikes`);
     return response.data;
   },
 };
